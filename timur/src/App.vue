9ea6b1077c692eb5d68493527ab4228b11b57470
Krevetych <timurbyvalcev95@gmail.com>
2024-04-09 10:43:50 +0400

    Added a ban on accessing the site from a non-mobile device.

--- timur/src/App.vue
+++ timur/src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { ref, computed } from 'vue'
 import BaseHeader from './components/BaseHeader.vue'
 import BaseFooter from './components/BaseFooter.vue'
+
+const windowWidth = ref(window.innerWidth)
+
+const isDeviceSizeExceedsPhone = computed(() => {
+	return windowWidth.value > 768
+})
+
+window.addEventListener('resize', () => {
+	windowWidth.value = window.innerWidth
+})
 </script>
 
 <template>
+	<div
+		v-if="isDeviceSizeExceedsPhone"
+		class="flex flex-col items-center justify-center h-screen gap-y-3"
+	>
+		<img src="/logo.png" alt="Logo" />
+		<p class="text-2xl font-semibold">
+			Данный сайт предназначен только для устройств с экраном меньше 768px
+		</p>
+		<p>Error code: E01</p>
+	</div>
 	<div class="flex flex-col justify-between h-screen">
 		<BaseHeader />
 		<router-view></router-view>
